5a7bcecdcb28dd4cb99043eb7acf84c05edb8b2b
Cannon-Cole <colecannonschool@gmail.com>
2022-09-12 04:22:28 -0600

    Set up db for authentication

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
 //ExpressJS
 const express = require("express");
 const app = express();
+var passport = require("passport");
+var LocalStrategy = require("passport-local");
+var crypto = require("crypto");
+//var db = require("../db");
+var logger = require("morgan");
+var passport = require("passport");
+var session = require("express-session");
+const mongoose = require("mongoose");
+const User = require("./models/Users");
 
-//Sockets.io
-const port = 3333;
-const server = app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-const { Server } = require("socket.io");
-const io = new Server(server);
+mongoose
+  .connect("mongodb://localhost:44444/authentication")
+  .then(() => {
+    console.log("DB connected");
+    // create a user a new user
+    var testUser = new User({
+      username: "Rigby",
+      password: "pass",
+    });
 
-let rooms = {};
+    // save the user to database
+    testUser.save(function (err) {
+      if (err) throw err;
+    });
+    //Sockets.io
+    const port = 3333;
+    const server = app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+    const { Server } = require("socket.io");
+    const io = new Server(server);
 
-app.use(express.static("public"));
+    let rooms = {};
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/index.html");
-});
+    app.use(express.static("public"));
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.broadcast.emit("chat message", socket.id + " connected");
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-    socket.broadcast.emit("chat message", socket.id + " disconnected");
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/public/index.html");
+    });
+
+    io.on("connection", (socket) => {
+      console.log("a user connected");
+      socket.broadcast.emit("chat message", socket.id + " connected");
+      socket.on("disconnect", () => {
+        console.log("user disconnected");
+        socket.broadcast.emit("chat message", socket.id + " disconnected");
+      });
+      socket.on("chat message", (msg) => {
+        console.log("message: " + msg);
+        socket.broadcast.emit("chat message", msg);
+      });
+    });
+  })
+  .catch(() => {
+    console.log("DB connection failed");
   });
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-    socket.broadcast.emit("chat message", msg);
-  });
-});

6b0dcc7d49acb6325072c48108e6b5c7a907ab4d
Cannon-Cole <colecannonschool@gmail.com>
2022-09-28 02:33:26 -0600

    Added basic create user and validation

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ var passport = require("passport");
 var session = require("express-session");
 const mongoose = require("mongoose");
 const User = require("./models/Users");
+const { db } = require("./models/Users");
 
 mongoose
   .connect("mongodb://localhost:44444/authentication")
   .then(() => {
     console.log("DB connected");
     // create a user a new user
-    var testUser = new User({
+    /*     var testUser = new User({
       username: "Rigby",
       password: "pass",
     });
@@ -24,7 +25,7 @@ mongoose
     // save the user to database
     testUser.save(function (err) {
       if (err) throw err;
-    });
+    }); */
     //Sockets.io
     const port = 3333;
     const server = app.listen(port, () => {
@@ -33,13 +34,43 @@ mongoose
     const { Server } = require("socket.io");
     const io = new Server(server);
 
-    let rooms = {};
-
     app.use(express.static("public"));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
     app.get("/", (req, res) => {
-      res.sendFile(__dirname + "/public/index.html");
+      res.sendFile(__dirname + "/public/html/create-account.html");
     });
+    app.post("/validate-and-create-account", (req, res) => {
+      let user = User.findOne(
+        { username: req.body.username_field },
+        function (err, result) {
+          if (err) {
+            console.log(err);
+          } else if (!result) {
+            var new_user = new User({
+              username: req.body.username_field,
+              password: req.body.password_field,
+            });
+            console.log(new_user);
+            // save the user to database
+            new_user.save(function (err) {
+              if (err) throw err;
+            });
+            console.log(result);
+          } else {
+            console.log(result);
+          }
+        }
+      );
+
+      res.send(
+        `username ${req.body.username_field} password ${req.body.password_field}`
+      );
+    });
+    /* app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/public/html/index.html");
+    }); */
 
     io.on("connection", (socket) => {
       console.log("a user connected");
